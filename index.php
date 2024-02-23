994ee55352c6a6f6ad1289e8a71e0960c5c24714
Alex Gthr <agauthier5@outlook.fr>
2024-02-23 12:00:07 +0100

    Ajout image et premiere requete teste sur index.php

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php 
     require_once('databaseConnect.php');
+    require_once('sql/requetesql.sql');
     ob_start();
+?>
+
+<p id="carousel">*********</br>ICI UN CAROUSSEL PLUS TARD DANS UN FUTUR PROCHE OU LOINTOIN</br>*********</p></div>
+
+<h1 id="titleContent">A l'affiche !</h1>
+
+<?php
+
+ // On recupère le recipes ma requete sql
+$afficheTitreFilm = $recipesAfficheEtTitreFilm->fetchAll();
+
+ // Et je l'affiche ici
+foreach($afficheTitreFilm as $afficheTitre) {
+
+    if ($afficheTitre['affiche'] == "") {
+        echo "";
+    }
+    
+    else {
+    echo "<img src='" . $afficheTitre['affiche'] . "'><br>",
+            "<p id='teste'>" . $afficheTitre['titre'] . "</p>";
+}}
+
 ?>
 
 <?php
